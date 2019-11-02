c964975724a8b9ecc226201db4756d817499b467
Maksim Tomilov <tomaks88@gmail.com>
2019-11-02 12:34:27 +0500

    Initial Commit

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by Максим Томилов on 02.11.2019.
+//  Copyright © 2019 Tomily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
