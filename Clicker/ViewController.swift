ed08800db66e1fbb02ed7600da402c103faaaaf7
Maksim Tomilov <tomaks88@gmail.com>
2019-11-05 21:58:43 +0500

    добавлено сохранение значение скликов. Добавлена кнопка сброса кликов

--- Clicker/ViewController.swift
+++ Clicker/ViewController.swift
@@ -13,19 +13,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var countClick: UILabel!
     private var click = 0
+    let userDefaults: UserDefaults = UserDefaults.standard
     
     @IBAction func clickButton(_ sender: Any) {
         click += 1
         countClick.text = "\(click)"
+        userDefaults.setValue(click, forKey: "clickValue")
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        countClick.text = "0"
         
+        countClick.adjustsFontSizeToFitWidth = true
+        countClick.minimumScaleFactor = 0.2
+        let saveClick = userDefaults.integer(forKey: "clickValue")
+        if saveClick != 0 {
+            click = saveClick
+        }
+        countClick.text = "\(click)"
     }
-
     
+    @IBAction func ClearClick(_ sender: Any) {
+        click = 0
+        countClick.text = "\(click)"
+    }
 }
 
